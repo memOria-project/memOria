258110b8a93e563fe6bca17e451db5ec731cf1e4
JHautier <jhautier@hotmail.fr>
2021-09-22 11:54:40 +0200

    refactor(Home): add keys and clean syntax

--- front/src/components/ListPaquets/index.js
+++ front/src/components/ListPaquets/index.js
@@ -1,12 +1,16 @@
 import Paquet from '../Paquet'
 
 const ListPaquets = ()=>{
-    const paquets = [{id:1, title:"javascript", created_at:1632302004, tag:"React"}, {id:2, title:"HTML", created_at:1632302005, tag:"Elements"}, {id:3, title:"CSS", created_at:16323020060, tag:"selectors"}];
+    const paquets = [
+        {id:1, title:"javascript", created_at:1632302004, tag:"React"}, 
+        {id:2, title:"HTML", created_at:1632302005, tag:"Elements"}, 
+        {id:3, title:"CSS", created_at:16323020060, tag:"selectors"}
+    ];
 
 return (
         <ul> 
             {paquets.map((paquet) => {
-                return <li> <Paquet paquet={paquet} /> </li>
+                return <li key={paquet.id}> <Paquet  paquet={paquet} /> </li>
             })}
         </ul>
         )
--- front/src/components/Paquet/index.js
+++ front/src/components/Paquet/index.js
@@ -1,16 +1,8 @@
-// const Paquet = ({paquet})=>{
-//     const {id, title, created_at, tag} = paquet;
-//     return (
-//     <p> id: {id} title: {title} created: {new Date(created_at*1000)} tag:{tag}</p>
-//     )
-// }
-
-
 const Paquet = ({paquet})=>{
-    const {id, title, created_at, timestamp, tag} = paquet;
-    console.log({id, title, created_at, timestamp, tag})
-    const date = Date(timestamp);
-    console.log(date);
+
+    const {id, title, created_at, tag} = paquet;
+    const date = Date(created_at);
+
     return (
     <p> id: {id} title: {title} created: {date} tag:{tag}</p>
     )
