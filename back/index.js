2b32ee90f7ce57d79cee7878b5bc113e5a1e992d
S.COULIBALY <s.coulibaly@outlook.com>
2021-09-23 23:02:34 +0200

    feat(Cors): Mise en place des cors

--- back/index.js
+++ back/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require("cors")
 
 const router = require('./app/router');
 
@@ -8,6 +9,9 @@ const app = express();
 
 const port = process.env.PORT || 5500;
 
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 app.use('/v1', router);
 
 
