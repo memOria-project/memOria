bd3e6a62f2bcabace48083b1fe3f7605b022959c
vmingam <vmingam@gmail.com>
2021-09-23 12:33:41 +0200

    fix(back): Exports pool from back/app/database.js

--- back/app/database.js
+++ back/app/database.js
@@ -5,9 +5,12 @@ const config= {
 };
 
 if (process.env.NODE_ENV === "production" ) {
+    //config pour la version de prod sur héroku, 
+    // ça évitera des messages d'erreur
     config.ssl= {
         rejectUnauthorized: false
     };
 }
 
 const pool = new Pool(config)
+module.exports = pool;
