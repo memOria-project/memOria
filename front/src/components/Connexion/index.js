5dc04963178270fa8c3f2eaccabbedc23c877523
yann-beraud <yann.beraud@gmail.com>
2021-09-24 13:26:13 +0200

    feat(authenticationState): mise en place reducer user

--- front/src/components/Connexion/index.js
+++ front/src/components/Connexion/index.js
@@ -5,20 +5,18 @@ import './Connexion-Desktop.scss'
 const Connexion= () => { 
 
   if (false) { return (
-    <div className="header__nav__connexion--connected">
-      <div className="login__user-profile-link">Utilisateur Lambda (profil)</div>
-      <a className="login__signout" href="#">Se déconnecter</a>
+    <div className="header__nav__connexion--disconnected">
+      <div className="login__signin">Se Connecter</div>
+      <div className="login__signup">S'inscrire</div>
     </div>
-    
-
-  )
+    )
 
   } else {
     return (
-    <div className="header__nav__connexion--disconnected">
-      <a className="login__signin" href="#" >Se Connecter</a>
-      <div className="login__signup">S'inscrire</div>
-    </div>
+      <div className="header__nav__connexion--connected">
+        <div className="login__user-profile-link">Utilisateur Lambda (profil)</div>
+        <div className="login__signout" >Se déconnecter</div>
+      </div>   
     )
   }
 }
