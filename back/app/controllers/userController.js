87f1a5ac7e8e7a5979e19e1ae51c713c6b66478c
S.COULIBALY <s.coulibaly@outlook.com>
2021-09-24 19:11:16 +0200

    Feat(crud-user-1): user login and get user infos feature implemented

--- back/app/controllers/userController.js
+++ back/app/controllers/userController.js
@@ -1,8 +1,30 @@
-
+const User= require("../models/user")
 const userController= {
-    subscribe: async function(request, response) {
-        return response.json({"user-subscribe": "ok"})
+
+    getOneUser: async function(request, response) {
+
+        try {
+            const user= await User.findOne(request.params.id);
+            response.status(200).json(user)
+        } catch (error) {
+            console.log(error);
+            response.status(500).json(error.message)
+        }
     },
+
+    login: async function(request, response) {
+
+        try {
+            const user = await new User(request.body).Login();
+            response.json(user);
+            console.log(user);
+        } catch(error) {
+            console.log(error);
+            response.status(500).json(error.message);
+        }
+    },
+
+
 }
 
 module.exports= userController;
