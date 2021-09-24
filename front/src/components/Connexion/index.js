f7ad236b1986ee5528691180d6f26cfa5f068892
yann-beraud <yann.beraud@gmail.com>
2021-09-24 11:35:37 +0200

    save(pageAccueil): save pageAccueil before develop pull

--- front/src/components/Connexion/index.js
+++ front/src/components/Connexion/index.js
@@ -1,21 +1,25 @@
 import './Connexion.scss'
 import './Connexion-Desktop.scss'
 
-//Affiche l'état de connexion de l'utilisateur dans la page d'accueil
+//Display user connexion state and associated options
 const Connexion= () => { 
 
   if (false) { return (
-  <div className="header__nav__connexion--disconnected">
-    <div className="login__signin">Se Connecter</div>
-    <div className="login__signup">S'inscrire</div>
-  </div>
+    <div className="header__nav__connexion--connected">
+      <div className="login__user-profile-link">Utilisateur Lambda (profil)</div>
+      <a className="login__signout" href="#">Se déconnecter</a>
+    </div>
+    
+
   )
 
   } else {
-    return (<div className="header__nav__connexion--connected">
-    <div className="login__user-profile-link">Utilisateur Lambda (profil)</div>
-    <div className="login__signout">Se déconnecter</div>
-  </div>)
+    return (
+    <div className="header__nav__connexion--disconnected">
+      <a className="login__signin" href="#" >Se Connecter</a>
+      <div className="login__signup">S'inscrire</div>
+    </div>
+    )
   }
 }
 
