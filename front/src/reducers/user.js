57ec3bf211baeb298454bd3e64caee897f2045d9
yann-beraud <yann.beraud@gmail.com>
2021-09-26 11:21:11 +0200

    CSS(pageAccueilMobile): blocage fonctionnement flexbox

--- front/src/reducers/user.js
+++ front/src/reducers/user.js
@@ -1,26 +1,16 @@
 import { ADMIN } from '../actions';
 import { DISCONNECT } from '../actions';
 
-<<<<<<< HEAD
-const initialState = {  name: false};
-=======
 const initialState = {
     isConnected: false,
     name: "",
     password:"",
 };
->>>>>>> 7df1c0e8f9c00e90ebfefcf6f74eec78b760f229
 
 //Handle actions on authentication
 const reducer = (state = initialState, action = {}) => {
   switch (action.type) {
     case ADMIN:
-<<<<<<< HEAD
-      return { name:"Administrateur"};
-
-    case DISCONNECT:
-      return {name: false};
-=======
       return {...state,
         isConnected: true,
         name: "Administrateur",
@@ -30,7 +20,6 @@ const reducer = (state = initialState, action = {}) => {
       return {...state,
         isConnected: false,
     };
->>>>>>> 7df1c0e8f9c00e90ebfefcf6f74eec78b760f229
 
     default:
       return state;
