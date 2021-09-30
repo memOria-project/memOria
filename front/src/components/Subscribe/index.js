dc75609cdd8d9bb69139d4b638347c50093e1249
JHautier <jhautier@hotmail.fr>
2021-09-30 12:02:23 +0200

    feat(Subscribe): add Errormessage and a few constraints on the form as test

--- front/src/components/Subscribe/index.js
+++ front/src/components/Subscribe/index.js
@@ -1,15 +1,31 @@
 import { useForm } from 'react-hook-form'
+import { ErrorMessage } from '@hookform/error-message'
 
-const Subscribe = ()=>{
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+const Subscribe = () => {
+  const { register, handleSubmit, watch, formState: { errors, isDirty, isValid } } = useForm()
+  
+  return <div>
+  <form onSubmit = {handleSubmit({type:"SUBSCRIBE"})}>
+   
+    <label> Nom d'utilisateur 
+      <input
+        {...register('name',
+          {
+            required: "Vous devez entrer un nom",
+            minLength:{value: 4, message:'Choisissez un nom dau moins 4 caractères'},
+            maxLength:{value: 15, message:'Choisissez un nom dau maximum 15 caractères'}
+        })} /> </label>
+    <ErrorMessage errors={errors} name="name" />
 
-return <form onSubmit = {handleSubmit}>
-  <label> Nom d'utilisateur <input {...register('name')} /> </label> <br />
-  <label> Email <input {...register('email')}/></label> <br />
-  <label> Mot de passe <input {...register('password')}/> </label> <br />
-  <label> Confirmez le mot de passe <input {...register('confirmPassword')}/></label> <br />
-  <button>J'ai déjà un compte</button>
-  <button type="submit">S'inscrire</button>
-</form>
+     <br />
+    <label> Email <input {...register('email')}/></label> <br />
+    <label> Mot de passe <input {...register('password')}/> </label> <br />
+    <label> Confirmez le mot de passe <input {...register('confirmPassword', { required: 'Vous devez soumettre un mot de passe'})}/></label> <br />
+    <button>J'ai déjà un compte</button>
+    <button type="submit">S'inscrire</button>
+  </form>
+  {JSON.stringify(watch())}
+
+  </div>
 }
 export default Subscribe
