578a57007dba83def4330cafd57c7bfab39f980d
Vincent Mingam <vmingam@gmail.com>
2021-09-30 10:16:34 +0200

    style(back-cards): Apply format corrections based on standard rules

--- back/app/models/card.js
+++ back/app/models/card.js
@@ -1,24 +1,27 @@
-const db= require("../database");
+const db = require('../database')
 
 class Card {
-    constructor(obj = {}) {
-        for (const propName in obj) {
-            this[propName] = obj[propName];
-        }
+  constructor (obj = {}) {
+    for (const propName in obj) {
+      this[propName] = obj[propName]
     }
+  }
 
-    static async cardsByUserId(userId) {
-        try {
-            const {rows} = await db.query('SELECT * FROM cards_of_user($1)',[userId]);
-            return rows.map(row => new Card(row));
-        } catch (error) {
-            if (error.detail) {
-                throw new Error(error.detail)
-            } else {
-                throw error
-            }
-        }
+  /**
+     * Get all cards owned by user (through deck possession)
+     */
+  static async cardsByUserId (userId) {
+    try {
+      const { rows } = await db.query('SELECT * FROM cards_of_user($1)', [userId])
+      return rows.map(row => new Card(row))
+    } catch (error) {
+      if (error.detail) {
+        throw new Error(error.detail)
+      } else {
+        throw error
+      }
     }
+  }
 
 }
-module.exports = Card;
+module.exports = Card
