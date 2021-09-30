242d5be8e69272361e44e3caeea3e17688556ef4
Vincent Mingam <vmingam@gmail.com>
2021-09-30 10:20:22 +0200

    style(back-cards): Apply standard linter

--- back/app/controllers/cardController.js
+++ back/app/controllers/cardController.js
@@ -1,17 +1,15 @@
-const Card= require("../models/card");
+const Card = require('../models/card')
 
 const cardController = {
-    getCardsFromUser: async (request, response) => {
-        try {
-            const cards = await Card.cardsByUserId(request.userId)
-            response.status(200).json(cards)
-        } catch (error) {
-            console.log(error)
-            response.status(500).json(error.message)
-        }
-
+  getCardsFromUser: async (request, response) => {
+    try {
+      const cards = await Card.cardsByUserId(request.userId)
+      response.status(200).json(cards)
+    } catch (error) {
+      console.log(error)
+      response.status(500).json(error.message)
     }
+  }
 }
 
-module.exports = cardController;
-
+module.exports = cardController
