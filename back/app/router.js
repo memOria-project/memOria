16b633097084cdd38cc1c95750d6d9ae5ed49f05
Vincent Mingam <vmingam@gmail.com>
2021-10-05 22:07:31 +0200

    doc (back): Add some more swagger API info

--- back/app/router.js
+++ back/app/router.js
@@ -19,20 +19,36 @@ router.get('/decks', deckController.getAllDecks) // Afficher tous les paquet
 router.post('/login', userController.login) // Connection de l’utilisateur
 router.get('/user/infos', checkJwt, userController.getOneUser)
 
+// Displays cards of a user
 /**
  * Respond with all cards of the connected user
  * @route GET /user/cards
  * @param{}
- * @returns {Array<Card>} 200 - An array of all cards
+ * @returns {Array<Deck>} 200 - An array of all cards
  * @returns {string} 500 - Server error
  */
-// Displays cards of a user
 router.get('/user/cards', checkJwt, cardController.getCardsFromUser)
 
 // Save or update card of a user
-router.post('/card', checkJwt, cardController.save )
+/**
+ * Respond with all cards of the connected user
+ * @route POST /cards
+ * @param{}
+ * @returns {integer<id>} 201 - An integer of card id
+ * @returns {} 204 - Empty response
+ * @returns {string} 500 - Server error
+ */
+router.post('/card', checkJwt, cardController.save)
 
-router.post('/signup', userController.subscribe) // Création  de compte utilisateur
+// Création  de compte utilisateur
+/**
+ * Respond with all cards of the connected user
+ * @route POST /signup
+ * @param{}
+ * @returns {integer<id>} 201 - An integer of user id
+ * @returns {string} 500 - Server error
+ */
+router.post('/signup', userController.subscribe)
 router.post('/user/update', checkJwt, userController.update) //  modification de compte utilisateur
 // router.delete("/user/:id", userController.remove); // Suppression d’un compte utilisateur
 // router.get("/logout", userController.disconnecte); // Déconnection de l’utilisateur
