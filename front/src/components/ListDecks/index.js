88dae6126c23d556b3fdc9956a1f8d2974942fcf
JMJHautier <jmjhautier@gmail.com>
2021-10-11 21:23:53 +0200

    style(Home): separate oclock decks from user decks

--- front/src/components/ListDecks/index.js
+++ front/src/components/ListDecks/index.js
@@ -3,20 +3,34 @@ import { useSelector } from 'react-redux';
 import './ListDecks.scss'
 import './ListDecks-Desktop.scss'
 import Loading from '../Loading';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 
 const ListDecks = ()=>{
     const allDecks = useSelector((state)=>(state.decks))
     const decks = allDecks.filter((deck)=> deck.card_number > 0)
+    const decksWithTags = allDecks.filter((deck)=>deck.tag !== null);
+    const oclockDecks= decksWithTags.filter((deck)=>deck.tag[0] === "oclock")
+    console.log(oclockDecks);
     console.log(decks);
-
-
-return (
+    const [openUser, setOpenUser] = useState(false)
+    const handleClick= () => {
+        setOpenUser((state)=>!state)
+    }
+return (<>
+        <div className="decks-container">
+            {decks.length>1?oclockDecks.map((deck) => {
+                return <Fragment key={deck.id}> <Deck  deck={deck} /> </Fragment>
+            }):<Loading />}
+        </div>
+        <h2 onClick={handleClick}> {openUser?"v":">"} Paquets de nos utilisateurs {openUser?"v":"<"}</h2> 
+        {openUser&&
         <div className="decks-container">
             {decks.length>1?decks.map((deck) => {
                 return <Fragment key={deck.id}> <Deck  deck={deck} /> </Fragment>
             }):<Loading />}
         </div>
+        }
+        </>
         )
 }
 export default ListDecks
