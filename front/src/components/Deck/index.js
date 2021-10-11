85c97cf9bc81bbfe246f8cd4377dc25744f30f2d
Vincent Mingam <vmingam@gmail.com>
2021-10-11 15:34:34 +0200

    refactor(front-deck): Cleanup nesting of decks in divs

--- front/src/components/Deck/index.js
+++ front/src/components/Deck/index.js
@@ -9,7 +9,7 @@ const Deck = ({ deck }) => {
   return (
     // je laisse ça ici au cas où on veut tester plus tard les autres données
     // <p className="deck"> id: {id} title: {title} created: {created_at} tag:{myTags}</p>
-  <p className="deck"> <NavLink to={path}>{title}</NavLink> </p>
+  <div className="deck"> <NavLink to={path}>{title}</NavLink> </div>
 
   )
 }
