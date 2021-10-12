8b71468347386102818cc74d2bf7b8240eff0415
JMJHautier <jmjhautier@gmail.com>
2021-10-12 18:21:25 +0200

    chore(Team): add comments

--- front/src/components/Team/index.js
+++ front/src/components/Team/index.js
@@ -1,15 +1,30 @@
 import ShowCards from "../CardDisplay/ShowCards"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 const Team = ()=>{
    // met tes données en **MarkDown** dans const database
    // 1 objet = 1 carte
    // n'utilise PAS les boutons OK et x, ça ne marchera pas. Utilise le bouton "au suivant"
-   
+
    const database = [
       {recto:"Souleymane", verso:"Le magnifique"},
       {recto:"monrecto", verso:"monverso"}
    ]
+   // tentative de disable les boutons. Mais ça ne marche pas, probablement car le DOM est impacté après.
+   // il faudrait utiliser useRef, mais il faudrait alors intervenir dans ShowCards... pas la peine vu l'utilité de la page
+   const warning = document.getElementsByClassName("warning");
+
+   useEffect(() => {
+   console.log(warning)   
+   warning&&warning[0].length===1?warning.style.disabled = true:console.log("pas de warning encore")
+   }, [warning])
+
+   // fin de la tentative
+
+   // failedCards est juste là pour remplir la props correspondante. Elle n'est aps utilisée ici.
+   const failedCards =[]
+
+   //cardId signifie en réalité "Card Index". On commence par 0, puis on remonte à  chaque "suivant"
    const [cardId, setCardId] = useState(0);
 
    const handleClick= () => {
@@ -19,7 +34,7 @@ const Team = ()=>{
 return <>
          <h2> L'équipe memOria</h2>
          <button onClick={handleClick}>Au suivant!</button>
-         <ShowCards cardId={cardId} database={database} failedCards={database} />
+         <ShowCards cardId={cardId} database={database} failedCards={failedCards} />
 
       </>
 }
