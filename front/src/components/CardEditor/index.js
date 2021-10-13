82544fc980c50ece08f2d51302fabb4865eebe3b
JMJHautier <jmjhautier@gmail.com>
2021-10-14 00:20:32 +0200

    --amend

--- front/src/components/CardEditor/index.js
+++ front/src/components/CardEditor/index.js
@@ -3,9 +3,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import Form from './Form.js'
-import './CardEditor.scss'
 import { EDIT_CARD, FETCH_CARDS, getCurrentDeckContent, POST_CARD } from "../../actions/index.js";
 import Confirmation from "./Confirmation.js";
+import './CardEditor.scss'
+import './CardEditor_Desktop.scss'
 
 const CardEditor = ()=>{
     let {deckId, cardId} = useParams()
@@ -15,17 +16,25 @@ const CardEditor = ()=>{
     const isModified = useSelector((state) => state.currentDeck.isModified)
     const [preview, setPreview] = useState(false)
     const [isSubmit, setIsSubmit] = useState(false)
-    const [isSuccess, setIsSuccess] = useState(false)
+    const [isSuccess, setIsSuccess] = useState() 
     const dispatch = useDispatch()
 
     useEffect( () => {
-      if(isModified){
-        setIsSuccess(true)
+      if(isModified.state){
+        setIsSubmit(false)
+
+        // setTimeout(()=> {setIsSubmit(false)}
+        // ,1000);
+        setTimeout(()=> {
+          setIsSubmit(true)
+          setIsSuccess(true)
+        }
+        ,100);
       }
       else{
         setIsSuccess(false)
       }
-      }, [isModified])
+      }, [isModified.count])
 
 
     dispatch({type:EDIT_CARD, field:[{"field":"currentDeckId",
@@ -43,20 +52,23 @@ const CardEditor = ()=>{
 
 return (
     <div>
-        {isSubmit?
+
+        <h1 className="cardEditor__title"> Créer /editer une carte </h1>
+        {isSubmit&&isSuccess!="init"?
           <Confirmation isSuccess={isSuccess} deckId={deckId} />
           :console.log("première soumission")
         }
-        <h1 className="title"> Créer /editer une carte </h1>
         <form id="recto" onSubmit={handleSubmit}> 
-            <label> Recto  
+          <div className="cardEditor__forms">
+            <label> 
             <Form isRecto={true} preview={preview}/>
             </label>
-            <label> Verso
+            <label> 
             <Form isRecto={false} preview={preview}/>
             </label>
-            <button onClick={handleClick}> Aperçu</button> 
-            <button type="submit">Créer</button>
+          </div>
+          <button className="information" onClick={handleClick}> {preview?"Retour éditeur":"Aperçu"}</button> 
+          <button type="submit" className="confirm">Créer</button>
         </form>
     </div>
     )
