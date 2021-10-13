729f23d3609de595b0691b4792fa02fc8d6286ca
JMJHautier <jmjhautier@gmail.com>
2021-10-14 00:19:46 +0200

    style(cardEditor): add styles for the confirmation and error messages and create animations

--- front/src/components/CardEditor/Confirmation.js
+++ front/src/components/CardEditor/Confirmation.js
@@ -1,13 +1,24 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { NavLink } from 'react-router-dom'
+import { motion } from 'framer-motion'
 
 const Confirmation = ({ isSuccess, deckId }) => {
   const path = `/deckEditor/${deckId}`
 return (isSuccess?
-        <div className="confirmation--Success"> 
-          Votre carte a bien été enregistrée. Soumettez une nouvelle carte, ou revenez au <NavLink to={path}>paquet</NavLink>
-        </div>
+        <motion.div
+          animate={{y:5}}
+          transition={{ease:"easeIn"}} 
+          className="confirmation--success"
+        > 
+         <FontAwesomeIcon icon={faCheck} /> <strong>Carte enregistrée!</strong> <br /> Revenez au <NavLink to={path}>paquet</NavLink> ou créer une autre carte
+        </motion.div>
         :
-        <div> Carte non créée. Veuillez réessayer. </div>
+        <motion.div
+        animate={{y:5}}
+        transition={{ease:"easeIn"}} 
+         className="confirmation--error"> 
+        <FontAwesomeIcon icon={faTimes} /> <strong>Carte non créée.</strong> <br /> Veuillez vous connecter et essayer à nouveau. </motion.div>
 )
 }
 export default Confirmation
