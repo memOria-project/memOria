837c23ae7f05b0527140f52748a8ed876702f945
JMJHautier <jhautier@hotmail.fr>
2021-10-28 10:48:36 +0200

    refactor(deckEditor): add comments, improve const names and lisibility overall

--- front/src/components/DeckEditor/index.js
+++ front/src/components/DeckEditor/index.js
@@ -1,68 +1,64 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { FETCH_CARDS, SET_CURRENT_DECK_ID, GET_CARD, DELETE_CARD, CHECK_TOKEN } from '../../actions'
 import { useEffect, useState } from 'react'
 import { NavLink, useParams } from 'react-router-dom'
 import MDEditor from '@uiw/react-md-editor'
-import './DeckEditor.scss'
-import Loading from '../Loading'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
+
+import Loading from '../Loading'
 import { handleClickEdit, handleClickDelete } from './handleClick'
-{ /* <FontAwesomeIcon icon="fa-solid fa-rectangle-vertical" /> */ }
+import { FETCH_CARDS, SET_CURRENT_DECK_ID, CHECK_TOKEN } from '../../actions'
+
+import './DeckEditor.scss'
 
 const DeckEditor = () => {
+  const deckIdFromParams = parseInt(useParams().deckId, 10)
+
   const [loading, setLoading] = useState(true)
   const [nameOfDeck, setNameOfDeck] = useState()
-  const { deckEditorDeckId } = useParams()
-  const deckIdFromParams = parseInt(deckEditorDeckId, 10)
+
+  const { deckId } = useSelector((state) => state.currentDeck)
+  const { currentDeck } = useSelector((state) => state)
+  const { cards } = currentDeck
+
+  const dispatch = useDispatch()
 
   useEffect(() => {
     dispatch({ type: CHECK_TOKEN })
     dispatch({ type: SET_CURRENT_DECK_ID, deckId: deckIdFromParams })
+    dispatch({ type: FETCH_CARDS })
   }, [])
-
-  const dispatch = useDispatch()
-  const { deckId } = useSelector((state) => state.currentDeck)
-  const currentDeckInEditor = useSelector(state => state.currentDeck).cards
+  // classement des cartes par id croissant
+  if (cards) {
+    cards.sort(function (a, b) { return a.id - b.id })
+  }
+  //! à supprimer quand #28 est résolu
   const { decks } = useSelector((state) => state.user)
-
   // ce  useEffect existe pour afficher le titre des paquets vides... pas très propre :( )
+  // pour le moment, il n'est pas possible de récupérer les titres des paquets vides, sauf en passant par le user state...
+
   useEffect(() => {
     if (decks.length) {
-      const findDeck = decks.find((deck) => deck.id === parseInt(deckEditorDeckId, 10))
+      const findDeck = decks.find((deck) => deck.id === deckIdFromParams)
       setNameOfDeck(findDeck.title)
       // const nameOfDeck = useSelector((state) => state.currentDeck.title)
     }
   }, [decks.length])
-
+  //! fin du code à supprimer quand #28 est résolu
+  //! ne pas oublier de remplacer nameOfDeck par currentDeck.title
   // ce use effect suffit pour les paquets avec des cartes.
-
+  // il pourra sans doute être améliorer quand #28 sera résolue
   useEffect(() => {
-    dispatch({ type: FETCH_CARDS })
-    if (!currentDeckInEditor && !currentDeckInEditor || currentDeckInEditor) {
-      setTimeout(() => setLoading(false), 1000)
+    if ((!cards && !cards) || cards) {
+      setTimeout(() => setLoading(false), 500)
     }
   }, [deckId])
 
-  if (currentDeckInEditor) {
-    currentDeckInEditor.sort(function (a, b) { return a.id - b.id })
-  }
-
-  // const handleClickDelete = (event) => {
-  //   console.log(event)
-  //   const cardId = event.target.id
-  //   dispatch({type:DELETE_CARD, cardId});
-
-  // }
-  // counter for cards
-  let count = 1
-  //
   return (
     <div>
       <div className="cardEditor__header">
         <div>
         <h2 className="header__title">{nameOfDeck}</h2>
-        {/* <h3>{currentDeckInEditor.length} cartes</h3> */}
         <FontAwesomeIcon icon={faEdit} />
         </div>
         <div className="header__newCard">
@@ -71,9 +67,8 @@ const DeckEditor = () => {
           </NavLink>
         </div>
       </div>
-       {/* <h2 className="header__undertitle">Cartes du paquet</h2>  */}
-      {currentDeckInEditor &&
-          (currentDeckInEditor.map((card) => {
+      {cards &&
+          (cards.map((card) => {
             return (
             <p key={card.id} className="rectoVersoView">
               <div className="card card__recto">
@@ -87,11 +82,10 @@ const DeckEditor = () => {
                   </pre>
               </div>
               <div className="card__title">
-                <p ><strong>Carte n°{count++}/{currentDeckInEditor.length}</strong></p>
+                <p ><strong>Carte n°{cards.indexOf(card) + 1}/{cards.length}</strong></p>
                 <NavLink to={`/cardEditor/${deckId}/${card.id}`}>
                   <button className="information" id={card.id} onClick={handleClickEdit}>
                 <FontAwesomeIcon icon={faEdit} size="2x" name={card.id} onClick={handleClickEdit}/>
-
                 </button>
                 </NavLink>
                 <button className="critic" id={card.id} onClick={handleClickDelete}>
@@ -101,7 +95,7 @@ const DeckEditor = () => {
             </p>)
           }
           ))}
-        {!currentDeckInEditor && !loading && <div style={{ marginTop: '2em', fontSize: '2em' }}> Ce paquet est vide! Vite, <NavLink to={`/cardEditor/${deckId}/new`}> ajoutez une carte!</NavLink> </div>}
+        {!cards && !loading && <div style={{ marginTop: '2em', fontSize: '2em' }}> Ce paquet est vide! Vite, <NavLink to={`/cardEditor/${deckId}/new`}> ajoutez une carte!</NavLink> </div>}
         {loading && <Loading />}
     </div>
   )
