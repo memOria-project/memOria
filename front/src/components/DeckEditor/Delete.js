da123b6543c3c83bc29074a7c6071d2170c058b0
JMJHautier <jhautier@hotmail.fr>
2021-10-29 10:41:26 +0200

    feat(deckEditor): require confirmation from user before deletion

--- front/src/components/DeckEditor/Delete.js
+++ front/src/components/DeckEditor/Delete.js
@@ -1,12 +1,23 @@
+import { useState } from 'react'
 import { PropTypes } from 'prop-types'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faTrash, faTrashRestoreAlt } from '@fortawesome/free-solid-svg-icons'
 import { handleClickDelete } from './handleClick'
+import classNames from 'classnames'
 
 const Delete = ({ card }) => {
+  const [isClicked, setIsClicked] = useState(false)
+  const buttonClass = classNames({
+    warning: !isClicked,
+    'btn-doubleCheck': isClicked
+  })
+
   return (
-   <button className="critic" id={card.id} onClick={handleClickDelete}>
-      <FontAwesomeIcon cardId={card.id} icon={faTrash} size="2x" style={{ cursor: 'pointer' }} />
+   <button className={buttonClass} id={card.id} onClick={(event) => handleClickDelete(event, isClicked, setIsClicked)}>
+     {isClicked
+       ? <FontAwesomeIcon className="btn-doubleCheck" icon={faTrashRestoreAlt} size="2x" />
+       : <FontAwesomeIcon cardId={card.id} icon={faTrash} size="2x" style={{ cursor: 'pointer' }} />
+     }
    </button>
   )
 }
