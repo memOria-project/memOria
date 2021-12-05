2ebb2848a44b5e6a764b14805079d1ff179b9251
Jean <jhautier@hotmail.fr>
2021-12-05 15:36:29 +0100

    feat(DOMPurify): create a clean and cleanObject functions and try them out with login

--- front/src/middlewares/auth.js
+++ front/src/middlewares/auth.js
@@ -1,23 +1,37 @@
 import { LOG_IN, UPDATE_USER, GET_USER, DELETE_TOKEN, DISCONNECT, UPDATE_SESSION, CHECK_TOKEN, SUBSCRIBE, UPDATE_PROFILE, REQUEST_SUCCESS } from '../actions'
+import DOMPurify from 'dompurify'
 
 const auth = (store) => (next) => (action) => {
   const { email, password } = store.getState().user
   const back = store.getState().back.url
   const token = localStorage.getItem('token')
+  const clean = (dirty) => {
+    const cleant = DOMPurify.sanitize(dirty)
+    console.log(cleant)
+    return cleant
+  }
+  const cleanObject = (object = ({ login: 'login', password: 'password <script></script>' })) => {
+    const cleantObject = Object.entries(object).map(([key, value]) => {
+      const cleantValue = clean(value)
+      console.log(cleantValue)
+      return [key, cleantValue]
+    }).reduce((previousValue, currentValue) => ({ [previousValue[0]]: previousValue[1], [currentValue[0]]: currentValue[1] }))
 
+    return cleantObject
+  }
   switch (action.type) {
     case LOG_IN: {
       const login = {
         password,
         email
       }
-
+      console.log(cleanObject(login))
       const options = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(login)
+        body: JSON.stringify(cleanObject(login))
       }
-      console.log('LOGIN', JSON.stringify(login))
+      console.log('LOGIN', JSON.stringify(cleanObject(login)))
 
       const getToken = async () => {
         try {
