8578419ed9537ecf036918a6054f6a60784685a1
JMJHautier <jhautier@hotmail.fr>
2021-12-16 00:00:33 +0100

    fix(cardDisplay): fix wrong syntax in filter causing bug in endgame

--- front/src/components/CardDisplay/removeFailedCards.js
+++ front/src/components/CardDisplay/removeFailedCards.js
@@ -1,6 +1,7 @@
 
 const removeFailedCard = (currentCard, failedCards, setFailedCards) => {
-  const filteredArray = failedCards.filter((card) => !card.recto === currentCard.recto && !card.verso === currentCard.verso)
+  const filteredArray = failedCards.filter((card) => card.recto !== currentCard.recto && card.verso !== currentCard.verso)
+  console.log({ failedCards, currentCard })
   setFailedCards(filteredArray)
 }
 
