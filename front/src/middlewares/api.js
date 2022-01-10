b29719f9996c94370b61c0d48c53d127541ca1b5
Jean <jhautier@hotmail.fr>
2022-01-10 21:00:11 +0100

    fix(deckEditor): fix route for DELETE_DECK action

--- front/src/middlewares/api.js
+++ front/src/middlewares/api.js
@@ -218,7 +218,7 @@ const api = (store) => (next) => (action) => {
       }
       const deleteDeck = async () => {
         try {
-          const request = await fetch(`${back}/deck`, options)
+          const request = await fetch(`${back}/deck/${action.deckId}`, options)
           const response = await request.status
           if (response === 200) {
             console.log(`deck supprimé: ${response}`)
