5ca1ddf1127039f7fd337b47756195857b8035de
Vincent Mingam <vmingam@gmail.com>
2022-03-20 15:11:06 +0100

    fix(back): reset cors restrictions

--- back/index.js
+++ back/index.js
@@ -7,10 +7,8 @@ const router = require('./app/router')
 const app = express()
 const port = process.env.PORT || 5500
 
-// TODO: DO NOT FORGET TO SET BACK TO RESTRICTED CORS POLICIES
-// const corsOption = { origin: process.env.ORIGIN_FRONT || "http://localhost:3000" }; // autorise seulement le domaine indiqué dans la variable ORIGIN_FRONT
-// app.use(cors(corsOption));// A placer le plus tôt possible et surtout toujours avant app.use('/v1', router);
-app.use(cors()) // Temporary CORS setup
+const corsOption = { origin: process.env.ORIGIN_FRONT || "http://localhost:3000" }; // autorise seulement le domaine indiqué dans la variable ORIGIN_FRONT
+app.use(cors(corsOption));// A placer le plus tôt possible et surtout toujours avant app.use('/v1', router);
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
